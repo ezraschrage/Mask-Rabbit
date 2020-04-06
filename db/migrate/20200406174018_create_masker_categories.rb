f8e6fb056eff8f043e507c1858018782a4f33d9a
ezschrage <ezschrage@gmail.com>
2020-04-06 13:54:07 -0400

    Tables written, pre migration

--- db/migrate/20200406174018_create_masker_categories.rb
+++ db/migrate/20200406174018_create_masker_categories.rb
@@ -2,6 +2,10 @@ class CreateMaskerCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :masker_categories do |t|
 
+      t.integer :masker_id, null: false
+      t.integer :category_id, null: false
+      t.integer :base_rate, null: false
+
       t.timestamps
     end
   end
--- db/migrate/20200406174025_create_masker_days.rb
+++ db/migrate/20200406174025_create_masker_days.rb
@@ -2,6 +2,10 @@ class CreateMaskerDays < ActiveRecord::Migration[5.2]
   def change
     create_table :masker_days do |t|
 
+      t.string :masker_id, null: false
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
+
       t.timestamps
     end
   end
