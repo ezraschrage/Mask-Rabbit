19c05c7f60149a5731db7ac0cc87bbb4f77b63f1
ezschrage <ezschrage@gmail.com>
2020-04-07 14:11:44 -0400

    Login and signup containers written, debugging

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Route } from 'react-router-dom'
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+// import GreetingContainer from "./greeting/greeting_container";
 
 const App = () => (
     <div>
         <h1>MaskRabbit App!</h1>
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
     </div>
 );
 
