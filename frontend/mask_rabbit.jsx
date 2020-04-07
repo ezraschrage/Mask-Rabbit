f06c87ff555fdb59db01c8c941e3f6d713abc78f
ezschrage <ezschrage@gmail.com>
2020-04-07 11:06:53 -0400

    User, session, and errors reducers, store, util written

--- frontend/mask_rabbit.jsx
+++ frontend/mask_rabbit.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
     const root = document.getElementById("root");
+
+//////TESTING
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+////////TESTING
+
     ReactDOM.render(<h1>Welcome to MaskRabbit!</h1>, root);
 });
