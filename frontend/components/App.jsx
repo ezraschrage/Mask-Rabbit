7e69cca449dc3f5581f388d9718a36a9eb3ed9f6
ezschrage <ezschrage@gmail.com>
2020-04-07 14:43:00 -0400

    Bootstrapping, deciding on layout

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route } from 'react-router-dom'
+import { Route, Redirect, Link, Switch } from 'react-router-dom'
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 // import GreetingContainer from "./greeting/greeting_container";
 
 const App = () => (
     <div>
         <h1>MaskRabbit App!</h1>
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+        <Switch>
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch>
     </div>
 );
 
--- frontend/mask_rabbit.jsx
+++ frontend/mask_rabbit.jsx
@@ -4,14 +4,25 @@ import configureStore from "./store/store";
 import Root from "./components/root"
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore();
-    const root = document.getElementById("root");
-
-//////TESTING
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
-
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
 ////////TESTING
+    window.getState = store.getState()
+    // window.dispatch = store.dispatch()
+    /////////TESTING
+
+    const root = document.getElementById('root');
 
 ReactDOM.render(<h1><Root store={store} /></h1>, root);
 });
