6e0ebbb6642e6e8b06c1043d4b5b42a992b9f5f2
ezschrage <ezschrage@gmail.com>
2020-04-09 14:57:30 -0400

    Category util and action written.

---
+++ frontend/actions/category_actions.js
@@ -0,0 +1,13 @@
+import * as CATEGORYUTIL from "../util/category_api_util";
+
+export const RECEIVE_CATEGORIES = 'RECEIVE_CATEGORIES';
+
+export const receiveCategories = (categories) =>( {
+    type: RECEIVE_CATEGORIES,
+    categories   
+});
+
+export const fetchCategories = () => dispatch => (
+    CATEGORYUTIL.fetchCategories().then(() =>
+    dispatch(receiveCategories(categories)))
+);
