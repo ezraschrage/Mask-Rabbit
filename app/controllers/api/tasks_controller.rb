c22a52b01ee915a192e1fe95bd1dc174131aae4a
ezschrage <ezschrage@gmail.com>
2020-04-14 10:19:49 -0400

    Task controller and routes modified

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,36 @@
 class Api::TasksController < ApplicationController
+    
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+       
+    end
+
+    def show
+        @task = Task.find(params[:id])      
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        if @task.destroy
+            render "api/tasks/show"
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:category_id, :length_of_task, :date, :per_hr,
+            :user_id, :masker_id, :description, :start_address, :end_address,
+            :vehicle_requirements
+        )
+    end
+
 end
