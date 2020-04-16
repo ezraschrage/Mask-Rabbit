f02df85e65e920f75b0cb31f0e8d2c568fd299d7
ezschrage <ezschrage@gmail.com>
2020-04-15 20:07:28 -0400

    Nav bar for task form completed. Only able to properly display for first page.

--- frontend/components/task_form/task_form_nav.jsx
+++ frontend/components/task_form/task_form_nav.jsx
@@ -23,46 +23,60 @@ class TaskFormNav extends React.Component {
                             <div className="form-nav-step">
                                 <div className="form-nav-step-top">
                                     <div className="form-nav-empty"></div>
-                                    <div className="form-nav-num">1</div>
+                                    <div className="form-nav-num first">
+                                        <span>1</span>
+                                    </div>
                                     <div className="form-nav-line"></div>
                                 </div>
-                                <div className="form-nav-bottom">
-                                    <span>Describe your task</span>
-                                </div>
-                            </div>
-                            <div className="form-nav-step">
-                                <div className="form-nav-step-top">
-                                    <div className="form-nav-line"></div>
-                                    <div className="form-nav-num">2</div>
-                                    <div className="form-nav-line"></div>
-                                </div>
-                                <div className="form-nav-bottom">
-                                    <span>Brose Maskers & prcices</span>
+                                <div className="form-nav-bottom first">
+                                    <span> Describe your task</span>
                                 </div>
                             </div>
                             <div className="form-nav-step">
                                 <div className="form-nav-step-top">
                                     <div className="form-nav-line"></div>
-                                    <div className="form-nav-num">3</div>
+                                    <div className="form-nav-num second">
+                                        <span>2</span>
+                                    </div>
                                     <div className="form-nav-line"></div>
                                 </div>
-                                <div className="form-nav-bottom">
+                                <div className="form-nav-bottom second">
+                                    <span>Browse Maskers & prices</span>
+                                </div>
+                            </div>
+                            <div className="form-nav-step">
+                                <div className="form-nav-step-top">
+                                    <div className="form-nav-line"></div>
+                                    <div className="form-nav-num third">
+                                        <span>3</span>
+                                    </div>
+                                    <div className="form-nav-line"></div>
+                                </div>
+                                <div className="form-nav-bottom third">
                                     <span>Choose date & time</span>
                                 </div>
                             </div>
                             <div className="form-nav-step">
                                 <div className="form-nav-step-top">
                                     <div className="form-nav-line"></div>
-                                    <div className="form-nav-num">4</div>
+                                    <div className="form-nav-num fourth">
+                                        <span>4</span>
+                                    </div>
                                     <div className="form-nav-empty"></div>
                                 </div>
-                                <div className="form-nav-bottom">
+                                <div className="form-nav-bottom fourth">
                                     <span>Confirm details</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="form-message"></div>
+                    <div className="form-bottom">
+                        <div className="form-message">
+                            <div className="form-message-notebook"></div>
+                            <div className="form-message-text">Tell us about your task. 
+                                We use these details to show Taskers in your area who fit your needs.</div>
+                        </div>
+                    </div>
                 </div>
             </>
         )
