73bf4b2eb3d9e147a19bbb9732029ec7b0682f27
ezschrage <ezschrage@gmail.com>
2020-06-01 20:25:24 -0400

    Root and entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import users from "./users_reducer";
 import categories from "./categories_reducer";
 import tasks from "./tasks_reducer";
+import maskers from "./maskers_reducer";
 
 const entitiesReducer = combineReducers({
     users,
     categories,
-    tasks
+    tasks,
+    maskers
 });
 
 export default entitiesReducer;
