25da7d1a4b9f2cfef0e1ec302292eb218342b7ca
ezschrage <ezschrage@gmail.com>
2020-06-06 13:25:45 -0400

    Footer other Links

--- frontend/components/footer/footer_component.jsx
+++ frontend/components/footer/footer_component.jsx
@@ -6,10 +6,17 @@ class Footer extends React.Component {
           <div className="footer">
             <div className="footer-txt">Follow us! We're friendly: </div>
             <div className="footer-link">
-              <a href="https://github.com/ezraschrage">
+              <a href="https://github.com/ezraschrage" target="_blank" rel="noopener noreferrer">
                   <button className="git"></button>
               </a>
               <div className="footer-break">|</div>
+              <a href="https://www.linkedin.com/in/ezraschrage/" target="_blank" rel="noopener noreferrer">
+                  <button className="linkedin"></button>
+              </a>
+              <div className="footer-break">|</div>
+              <a href="https://angel.co/u/ezra-schrage" target="_blank" rel="noopener noreferrer"> 
+                  <button className="angellist"></button>
+              </a>
             </div>
           </div>
 
