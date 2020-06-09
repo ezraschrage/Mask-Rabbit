6618af6a812ad8c7d388cd802047847be5bc3f7b
ezschrage <ezschrage@gmail.com>
2020-06-09 11:49:15 -0400

    Delete Button

--- frontend/components/tasks/tasks_item.jsx
+++ frontend/components/tasks/tasks_item.jsx
@@ -4,64 +4,21 @@ import { Link } from 'react-router-dom';
 class TasksItem extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            masker_first_name: this.props.masker.first_name,
-            masker_last_name: "",
-            masker_photourl: "",
-            masker_bio: this.props.masker.bio
-        }
 
-        this.maskerInfo = this.maskerInfo.bind(this)
+        this.deleteButton = this.deleteButton.bind(this)
     }
 
-
-    componentDidMount() {
-        // console.log(this.props.task.masker_id)
-    //     console.log(this.props)
-        // this.props.fetchMasker(this.props.task.masker_id)
-        // return this.setState({ masker_first_name: this.props.masker.first_name })
-    //     // console.log(this.props.masker)
-    //     // console.log(this.state)
-    //     this.setState({
-    //         masker_first_name: this.props.masker.first_name,
-    //         masker_last_name: this.props.masker.last_name,
-    //         masker_photourl: this.props.masker.photoUrl,
-    //         masker_bio: this.props.masker.bio
-    //     })
-    //     console.log(this.state)
-    }
-
-    maskerInfo (masker_id) {
-
-    //     // console.log(this.props.masker)
-    //     // console.log(this.props.state)
-    //     // if (this.props.props.maskers[masker_id] === undefined){
-    //         this.props.fetchMasker(masker_id)
-    //     // }
-    //     // this.setState({ masker })
-    //     this.setState({ masker_first_name: this.props.masker.first_name, 
-    //         masker_last_name: this.props.masker.last_name, 
-    //         masker_photourl: this.props.masker.photoUrl,
-    //         masker_bio: this.props.masker.bio})
-    //     console.log(this.props)
-    //     console.log(this.state)
-    }
-
-
+    deleteButton(e) {
+        e.preventDefault();
+        this.props.deleteTask(this.props.task.id)
+    } 
 
     render() {
-        
-        // console.log(this.props)
-        // console.log(this.state)
-        // const mask = this.props.fetchMasker(this.props.task.masker_id)
-        // console.log(mask)
         let dateArray = this.props.task.date.split('-')
         let date = new Date(parseInt(dateArray[0]), parseInt(dateArray[1]) - 1,
             parseInt(dateArray[2]))
         let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         let prettyDate = new Intl.DateTimeFormat('en-US', options).format(date)
-        console.log(this.props.masker)
-        console.log(this.state)
     return (
         <div className="task-li-container">
             <div>
@@ -84,6 +41,9 @@ class TasksItem extends React.Component {
                     <div><u className="underline">Bio</u>: {this.props.task.bio}</div>
                 </div>
                 <img className="Masker-pic-task" src={this.props.task.photoUrl} />
+            </div>
+            <div className="task-cancel">
+                <button className="account-btn cancel" onClick={(e) => this.deleteButton(e)}>Cancel Task</button>
             </div>
         </div>
     )
